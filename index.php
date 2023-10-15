2a2f679e0c913c1f14fbbd05e26875c7b5164b61
Ehsan Jafari <ehs.jafari20@gmail.com>
2023-10-15 18:24:23 +0330

    feat: adding helper function to project
    Improve project maintenance and avoid redundancy code by adding helper function like protocole, currentDomain, asset, url, currentUrl.

--- index.php
+++ index.php
@@ -14,3 +14,27 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 
 // Helpers
+function protocole(){
+    return stripos($_SERVER['SERVER_PROTOCOL'], 'https') === true ? "https://" : "http://";
+}
+
+function currentDomain(){
+    return protocole() . $_SERVER['HTTP_HOST']; // https://ehsannjafari.com
+}
+
+function asset($src){
+    $domain = trim(CURRENT_DOMAIN,'/ ');
+    $src = $domain . '/' . trim($src,'/ ');
+    return $src;
+}
+
+function url($url){
+    $domain = trim(CURRENT_DOMAIN,'/ ');
+    $url = $domain . '/' . trim($url,'/ ');
+    return $url;
+}
+
+# current opened url : use for routing system
+function currentUrl(){
+    return currentDomain() . $_SERVER['REQUEST_URI'];
+}
